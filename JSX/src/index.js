2ec73182b9c81bdc714cee0404a62d8771467ce0
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2024-05-03 02:15:35 +0530

    Add files via upload

---
+++ JSX/src/index.js
@@ -0,0 +1,17 @@
+//Create a react app from scratch.
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+ReactDOM.render(
+  <div>
+    <h1> My Name is </h1>
+    <ul>
+      <li>Syeda</li>
+      <li> Suha</li>
+      <li>Sana</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
